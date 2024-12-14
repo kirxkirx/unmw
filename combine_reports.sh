34dc94e8d629341a1660b05b684df4c77e5ce54a
Kirill Sokolovsky <kirx@kirx.net>
2024-12-15 00:26:49 +0300

    report the largest of the two offsets in the combined log

--- combine_reports.sh
+++ combine_reports.sh
@@ -327,7 +327,8 @@ Reports on the individual fields may be found at $URL_OF_DATA_PROCESSING_ROOT/au
  # sed is for the case Record 39: "TIMESYS = 'UTC     '           / Default time system" status=0 to avoid 'UTC
  TIMESYS_OF_LAST_IMAGE_DATE=`grep 'time system' "$INPUT_DIR/index.html" | head -n1 | awk '{print $5}' | sed "s:'::g"`
  LAST_IMAGE_DATE="$LAST_IMAGE_DATE $TIMESYS_OF_LAST_IMAGE_DATE"
- IMAGE_CENTER_OFFSET_FROM_REF_IMAGE=`grep 'Angular distance between the image centers' "$INPUT_DIR/index.html" | sed 's:deg.::g' | head -n1 | awk '{print $7}'`
+ #IMAGE_CENTER_OFFSET_FROM_REF_IMAGE=`grep 'Angular distance between the image centers' "$INPUT_DIR/index.html" | sed 's:deg.::g' | head -n1 | awk '{print $7}'`
+ IMAGE_CENTER_OFFSET_FROM_REF_IMAGE=$(grep 'Angular distance between the image centers' "$INPUT_DIR/index.html" | awk '{if($7+0 > max) max=$7} END{print max}')
  MAG_LIMIT=`grep 'All-image limiting magnitude estimate' "$INPUT_DIR/index.html" | tail -n1 | awk '{print $5}'`
  # remove "UTC" as we have it in the table header
  echo -n "<tr><td>$CAMERA</td><td>${LAST_IMAGE_DATE/ UTC/}</td><td><font color='teal'> $FIELD </font></td><td>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;</td>" >> "$OUTPUT_PROCESSING_SUMMARY_HTML_NAME"
