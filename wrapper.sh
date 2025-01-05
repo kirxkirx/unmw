6350e6b7f6a8284c465a18c479f80716b6a65fe4
Kirill Sokolovsky <kirx@kirx.net>
2025-01-05 23:06:37 +0300

    update local_config.sh_example

--- wrapper.sh
+++ wrapper.sh
@@ -6,12 +6,12 @@ if [ -s local_config.sh ];then
  source local_config.sh
 fi
 
-# You may specify an external plate-solve server here
-# but better move it to local_config.sh
-if [ -z "$ASTROMETRYNET_LOCAL_OR_REMOTE" ];then
- ASTROMETRYNET_LOCAL_OR_REMOTE="local"
- #export ASTROMETRYNET_LOCAL_OR_REMOTE="remote"
- #export FORCE_PLATE_SOLVE_SERVER="scan.sai.msu.ru"
-fi
+## You may specify an external plate-solve server here
+## but better move it to local_config.sh
+#if [ -z "$ASTROMETRYNET_LOCAL_OR_REMOTE" ];then
+# ASTROMETRYNET_LOCAL_OR_REMOTE="local"
+# #export ASTROMETRYNET_LOCAL_OR_REMOTE="remote"
+# #export FORCE_PLATE_SOLVE_SERVER="scan.sai.msu.ru"
+#fi
 
 ./autoprocess.sh $1 &>/dev/null &
